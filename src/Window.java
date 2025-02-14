6e34cef2f1a6be5a1cedd98d8b5f2d096c2460a7
Andrew Nolan <andrewnolan871@gmail.com>
2025-02-14 21:12:14 +0000

    haven't broken anything. but tried to add mouselistener and it's not working, but I'll get it to work.

--- src/Window.java
+++ src/Window.java
@@ -11,19 +11,24 @@
  import javax.swing.*;
  
 
-public abstract class Window extends JPanel implements ActionListener {
+public abstract class Window extends JPanel implements ActionListener, MouseListener  {
     public static final int WIDTH = 800;
     public static final int HEIGHT = 800;
     private Timer timer;
     public List<Body> bodies;
     
+    // need for drawing tree and making text
     Tree tree;
     double lastTime;
     double currentTime;
     double fps;
-
     public boolean drawTree = false;
     
+    // need for drawing arrows for adding particles through GUI
+    private Point startPoint = null;
+    private Point endPoint = null;
+    private boolean drawingArrow = false;    
+
     public Window(List<Body> bodies, double fps) {
         this.timer = new Timer(utils.framesPerSecondToMillisecondsPerFrame(fps), this); // Roughly 60 FPS
         this.bodies = bodies;
@@ -53,6 +58,7 @@ public abstract class Window extends JPanel implements ActionListener {
         super.paintComponent(g);
         
         // Set background color to black
+        // Graphics2D g = (Graphics2D) g;
         g.setColor(Color.BLACK);
         g.fillRect(0, 0, getWidth(), getHeight());
     
@@ -69,7 +75,52 @@ public abstract class Window extends JPanel implements ActionListener {
         for (Body body : this.bodies) {
             body.draw(g);
         }
+        // Draw arrow if active
+        // Draw the slingshot arrow if we're currently dragging
+        if (drawingArrow && startPoint != null) {
+            Point current = getMousePosition(); // current mouse pos relative to this panel
+            if (current != null) {
+                Graphics2D g2 = (Graphics2D) g;
+                drawArrow(g2, startPoint.x, startPoint.y, current.x, current.y);
+            }
+        }      
     }
+    // drawing arrows for adding particles through GUI
+    private void drawArrow(Graphics2D g, int x1, int y1, int x2, int y2) {
+        g.drawLine(x1, y1, x2, y2);
+    
+        double angle = Math.atan2(y2 - y1, x2 - x1);
+        int arrowSize = 10;
+    
+        int xArrow1 = x2 - (int) (arrowSize * Math.cos(angle - Math.PI / 6));
+        int yArrow1 = y2 - (int) (arrowSize * Math.sin(angle - Math.PI / 6));
+        int xArrow2 = x2 - (int) (arrowSize * Math.cos(angle + Math.PI / 6));
+        int yArrow2 = y2 - (int) (arrowSize * Math.sin(angle + Math.PI / 6));
+    
+        g.drawLine(x2, y2, xArrow1, yArrow1);
+        g.drawLine(x2, y2, xArrow2, yArrow2);
+    }
+    
+    // MouseListener methods
+    @Override
+    public void mousePressed(MouseEvent e) {
+        System.out.println("Mouse pressed");
+        startPoint = e.getPoint();
+        drawingArrow = true;
+        repaint();
+    }
+    
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        // endPoint = e.getPoint();
+        drawingArrow = false;
+        repaint();
+        // add mew body here?
+    }
+    
+    @Override public void mouseClicked(MouseEvent e) { }
+    @Override public void mouseEntered(MouseEvent e) { }
+    @Override public void mouseExited(MouseEvent e) { }
 }
     
 
@@ -77,4 +128,5 @@ class utils{
     public static int framesPerSecondToMillisecondsPerFrame(double fps) {
         return (int) (1000.0 / fps);
     }
+    
 }
